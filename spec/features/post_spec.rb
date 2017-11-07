670f3a617177810e413e9e5ce7164d333ee548b8
Yohanan Assefa <nanahoy@gmail.com>
2017-11-06 21:05:46 -0700

    Implement user association with posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -17,6 +17,10 @@ describe 'navigate' do
 
   describe "creation" do
     before do
+      user =  User.create(email: 'test@test.com', password: 'asdfasdf',
+                          password_confirmation: 'asdfasdf', first_name: "John",
+                          last_name: 'Snow')
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -25,13 +29,21 @@ describe 'navigate' do
     end
 
     it "can be created from new form page" do
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "some rationale"
 
       click_on "Save"
 
       expect(page).to have_content("some rationale")
+    end
+
+    it "will have a user associated with it" do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Something")
+      user = User.create(email: 'test@test.com', password: 'asdfasdf',
+                            password_confirmation: 'asdfasdf', first_name: "John",
+                            last_name: 'Snow')
+      @post = user.posts.create(date: Date.today, rationale: "Something")
     end
 
     it "can be created" do
