72f99c7a533ab8493d07b48c76a0995c6b991c81
Yohanan Assefa <nanahoy@gmail.com>
2017-11-08 12:09:47 -0700

    Implement enum with status for approval work flow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates_presence_of :date, :rationale
   belongs_to :user
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107034814) do
+ActiveRecord::Schema.define(version: 20171108184311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171107034814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
