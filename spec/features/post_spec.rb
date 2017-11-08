d1e8f208ff460c81e0c395ddbf4a9af3327ec948
Yohanan Assefa <nanahoy@gmail.com>
2017-11-07 19:40:06 -0700

    Implement ability to delete post records

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -60,6 +60,16 @@ describe 'navigate' do
     end
   end
 
+  describe "delete" do
+    it "can be deleted" do
+      post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
