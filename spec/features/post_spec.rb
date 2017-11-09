ce5c9866131a961d0526c9ae26452506e517c642
Yohanan Assefa <nanahoy@gmail.com>
2017-11-08 17:38:07 -0700

    Implement page redirection for non authorized users

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -72,14 +72,7 @@ describe 'navigate' do
 
   describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
-    end
-
-    it "can be reached by clicking edit on the index page" do
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
+      @post = FactoryGirl.create(:post, user_id: @user.id)
     end
 
     it "can be edited" do
@@ -90,6 +83,16 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Edited content")
+    end
+
+    it "cannot be edited by a non authorized user" do
+      logout(:user)
+      non_authorized_user = create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
