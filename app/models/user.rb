9d606d92142310f019c6b1b47af100043aa7b618
Yohanan Assefa <nanahoy@gmail.com>
2017-11-09 15:54:03 -0700

    Created resource for audit log with model relationship connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :phone
   validates :phone, length: { is: 10 }
   has_many :posts
+  has_many :audit_logs
 
   PHONE_REGEX = /\A[0-9]*\Z/
 
