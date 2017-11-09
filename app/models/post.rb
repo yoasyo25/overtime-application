cd2e97c15e25c2ab1744cf8fab36a621b9fb59a4
Yohanan Assefa <nanahoy@gmail.com>
2017-11-08 20:31:30 -0700

    Implement custom scope for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates_presence_of :date, :rationale
   belongs_to :user
   enum status: { submitted: 0, approved: 1, rejected: 2 }
+
+  scope :posts_by, ->(user) { where(user_id: user.id) }
 end
