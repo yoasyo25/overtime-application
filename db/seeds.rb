addf9ead79bfb8aa4a260bdf0f0bdccfdc41179c
Yohanan Assefa <nanahoy@gmail.com>
2017-11-10 10:05:40 -0700

    Add live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,17 @@ puts "1 Admin created"
 puts "100 Audit logs created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post + 1}: Posting",
-               user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post + 1}: Justo tortor maecenas
+              eu non litora diam, pretium accumsan malesuada nunc dictum.
+              Vehicula tellus ligula, donec vestibulum, vitae facilisis
+              vulputate, sit mauris duis sodales orci. Quam neque, etiam quis,
+              quam magna voluptate nunc justo, parturient eros magnis fusce
+              venenatis, dolor nisl. Varius hymenaeos commodo erat, eu quis
+              nam malesuada leo vitae. Eu taciti. Neque nunc faucibus vestibulum
+              odio, laoreet amet, nam ultricies morbi aenean id nunc, vestibulum
+              cubilia risus amet vel, vitae eget gravida curabitur nec tempus.
+              Dapibus dignissim suscipit, et convallis.",
+              user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
