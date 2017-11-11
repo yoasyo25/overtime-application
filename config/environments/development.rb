187f6fcf41634e9ccb1db473cc6de174ef524386
Yohanan Assefa <nanahoy@gmail.com>
2017-11-10 18:16:13 -0700

    Integrate basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
@@ -21,4 +20,6 @@ Rails.application.configure do
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
